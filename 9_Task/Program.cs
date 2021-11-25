a4a930e8178626e98bd1e501c4e23d429324aa2c
Администратор <@DESKTOP-PJPICF5>
2021-11-25 23:19:02 +0800

    Добавьте файлы проекта.

---
+++ 9_Task/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+try
+{
+    Console.WriteLine("Калькулятор в C#\r");
+    Console.WriteLine("Введите число и нажмите Enter");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число и нажмите Enter");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Выберите вариант из списка:");
+    Console.WriteLine("\ta - Сложить");
+    Console.WriteLine("\ts - Вычесть");
+    Console.WriteLine("\tm - Умножить");
+    Console.WriteLine("\td - Разделить");
+    switch (Console.ReadLine())
+    {
+        case "a":
+            Console.WriteLine($"Результат: {0} + {1} = " + (number1 + number2));
+            break;
+        case "s":
+            Console.WriteLine($"Результат: {0} - {1} = " + (number1 - number2));
+            break;
+        case "m":
+            Console.WriteLine($"Результат: {0} * {1} = " + (number1 * number2));
+            break;
+        case "d":
+            Console.WriteLine($"Результат: {0} / {1} = " + number1 / number2);
+            break;
+        default:
+            Console.WriteLine("Нет операции с указанным номером");
+            break;
+    } 
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.Write("Нажмите любую клавишу, чтобы закрыть калькулятор...");
